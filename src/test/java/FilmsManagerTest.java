4a24114258b60036db05e69a282a6d1d12a51073
user test <klimchuk.alieksandr.1987@mail.ru>
2023-08-20 18:19:28 +0300

    first commit

--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -58,4 +58,25 @@ public class FilmsManagerTest {
         String[] expected = {};
         Assertions.assertArrayEquals(expected, actual);
     }
+
+        @Test
+        public void findLastMore() {
+            FilmsManager manager = new FilmsManager(10);
+            manager.addMovie("Film 1");
+            manager.addMovie("Film 2");
+            manager.addMovie("Film 3");
+            manager.addMovie("Film 4");
+            manager.addMovie("Film 5");
+            manager.addMovie("Film 6");
+            manager.addMovie("Film 7");
+            manager.addMovie("Film 8");
+            manager.addMovie("Film 9");
+            manager.addMovie("Film 10");
+            manager.addMovie("Film 11");
+
+            String[] expected = {"Film 11", "Film 10", "Film 9", "Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+            String[] actual = manager.findLast();
+
+            Assertions.assertArrayEquals(expected, actual);
+    }
 }
